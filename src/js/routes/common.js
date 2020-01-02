c33609ee1b11a669d3867e43a5ee0fefda0c8a0b
nolafs <Sontag2010>
2020-01-02 13:05:18 +0000

    - mobile nav added

--- src/js/routes/common.js
+++ src/js/routes/common.js
@@ -3,29 +3,29 @@ import $ from "jquery";
 export default {
   init() {
 
-    let p_scroll=0;
-    if($(window).scrollTop() > 10){
-      setTimeout(() => {
-        $('.top-bar').addClass('is-stuck')
-      }, 500)
-    }
+    let p_scroll = 0;
+
+    setTimeout(() => {
     $(window).scroll(function () {
-      if($(window).scrollTop() >  10) {
-        $('.top-bar').addClass('is-stuck')
-      } else {
-        $('.top-bar').removeClass('is-stuck')
-      }
-      if($(window).scrollTop() > 10) {
-        if ($(window).scrollTop() < p_scroll) {
-          $('.top-bar').addClass('scroll-up')
-          $('.top-bar').removeClass('scroll-down')
+      if($(window).scrollTop() !== 0) {
+        if ($(window).scrollTop() > 10) {
+          $('.top-bar').addClass('is-stuck')
         } else {
-          $('.top-bar').addClass('scroll-down')
-          $('.top-bar').removeClass('scroll-up')
+          $('.top-bar').removeClass('is-stuck')
         }
+        if ($(window).scrollTop() > 10) {
+          if ($(window).scrollTop() < p_scroll) {
+            $('.top-bar').addClass('scroll-up')
+            $('.top-bar').removeClass('scroll-down')
+          } else {
+            $('.top-bar').addClass('scroll-down')
+            $('.top-bar').removeClass('scroll-up')
+          }
+        }
+        p_scroll = $(window).scrollTop();
       }
-      p_scroll = $(window).scrollTop();
     })
+    }, 500)
 
   },
   finalize() {
