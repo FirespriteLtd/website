a5541e1a5e4ecd628f3ae40af9f921e05f513561
nolafs <Sontag2010>
2020-01-14 12:13:31 +0000

    - fixed scrollmagic and latest gasp

--- src/js/routes/home.js
+++ src/js/routes/home.js
@@ -3,13 +3,14 @@ import $ from 'jquery';
 import 'slick-carousel';
 import Rellax from 'rellax/rellax.min';
 import YTPlayer from 'yt-player';
+import { gsap, TweenMax, TweenLite, TimelineMax, Expo } from 'gsap/all';
 
 import ScrollMagic from 'scrollmagic/scrollmagic/minified/ScrollMagic.min';
 import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min';
-
-import { gsap, TweenMax, TimelineMax, Expo } from 'gsap/all';
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+
+
 
 import { SplitText } from 'gsap/SplitText';
 import animations from "../util/animations"
@@ -26,95 +27,24 @@ export default {
         const title = new SplitText($('.hero-header-inner').find('h1'), {type:"words,chars"});
         const chars = title.words;
 
-        tl.staggerFrom(chars, 0.8, {opacity:0, scaleY: 0, y:80,  ease:Expo.easeOut}, 0.1, "+=0");
-        tl.from($('.content-subtitle-animate'), 0.5,  {opacity:0, y: 100, ease:Expo.easeOut});
+        tl.from(chars, {opacity:0, scaleY: 0, y:80, duration: 0.8,  ease:Expo.easeOut, stagger: 0.1});
+        tl.from($('.content-subtitle-animate'),{opacity:0, duration:0.5, y: 100, ease:Expo.easeOut});
 
         const anim = new ScrollMagic.Scene({
             triggerElement: heading,
             triggerHook: 0.4
         })
-            /*
+
             .addIndicators({
                 name: "Heading Timeline",
                 colorTrigger: "green",
                 colorStart: "red",
                 colorEnd: "black"
             })
-            */
+
             .setTween(tl)
             .addTo(controller)
 
-    },
-    animServices() {
-
-        const tl = gsap.timeline();
-        const heading = $('.home-service-heading');
-        tl.set(heading.find('.h-anim'), {x:'100%'})
-        tl.from(heading.find('h2'), 0.3, {
-           y: '-100%'
-        });
-
-        tl.to(heading.find('.h-anim'), 0.5, {
-            x: '0%'
-        });
-        tl.from($('.h-anim-2'), 0.5, {
-            x: '100%',
-            opacity:0
-        },'-=0.5');
-        tl.from($('.home-service-content'), 0.5, {
-            y: 100,
-            opacity: 0
-        });
-
-        const anim = new ScrollMagic.Scene({
-            triggerElement: heading,
-            offset: 100,
-            triggerHook: 0.5
-        })
-            /*
-            .addIndicators({
-                name: "Services Timeline",
-                colorTrigger: "black",
-                colorStart: "black",
-                colorEnd: "black"
-            })
-             */
-            .setTween(tl)
-            .addTo(controller);
-
-    },
-    animList () {
-
-
-
-        $(".block-icon-list li").each(function(i) {
-            const tl = gsap.timeline();
-            tl.from($(this).find('.item'), 0.5, {
-                y: '30%',
-                opacity: 0
-            });
-            tl.from($(this).find('.item > img'),0.5, {rotationY:90, y:-10, opacity:0 }, '=-0.4')
-
-
-         new ScrollMagic.Scene({
-            triggerElement: this,
-             triggerHook: 0.5
-        })
-            /*
-            .addIndicators({
-                name: "Box Timeline",
-                colorTrigger: "black",
-                colorStart: "black",
-                colorEnd: "black"
-            })
-             */
-            .setTween(tl)
-            .addTo(controller);
-        });
-    },
-    animBlocks() {
-        const blocks = ['#case-studies', '#clients', '#testimonial', '#block-awards', '#block-contact'];
-       animations.animBlock(controller,blocks);
     },
     init() {
         // JavaScript to be fired on all pages
@@ -140,7 +70,7 @@ export default {
             player.play();
         })
 
-
+        this.animHeader();
         /*
         var configProfile = {
             "profile": {"screenName": 'RenaissancePRUK'},
@@ -154,7 +84,7 @@ export default {
         };
         twitterFetcher.fetch(configProfile);
 
-        //this.animHeader();
+
         //this.animList();
         //this.animServices();
         //this.animBlocks();
