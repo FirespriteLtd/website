2e060da79c5bd10081128bc81898c6fd08ffa232
nolafs <Sontag2010>
2020-01-15 14:05:10 +0000

    - fixed scrollmagic and latest gasp

--- src/js/util/videoBlock.js
+++ src/js/util/videoBlock.js
@@ -27,6 +27,7 @@ class VideoBlock {
         if(this.player){
           this.animation(id);
           this.pin(id);
+          this.introAnimation(id)
         }
       }
 
@@ -104,15 +105,41 @@ class VideoBlock {
   revealAnim(id){
     const header = this.block.find('.header-content');
     const headerButton = this.block.find('.header-button a');
-    const tl = gsap.timeline({repeat:0, delay: 1.5});
-    const title = new SplitText(header.find('h3'), {type:"words,chars"});
-    const chars = title.words;
-    tl.from(header.find('h2'), {opacity:0, y:'30%', scale:2, duration: 0.5, ease:Expo.easeOut}, '-=0.5');
-    tl.from(chars, {opacity:0, scaleY: 0, y:80, duration: 0.8,  ease:Expo.easeOut, stagger: 0.1});
+    const tl = gsap.timeline({repeat:0, delay: 0});
 
     tl.fromTo(this.block.find('.revealCover'), {x:'-100%'}, {x:'200%', duration: 5})
     tl.from(headerButton, {opacity:0, y: '100%', duration:1})
     return tl;
+  }
+
+  introAnimation(id){
+
+    const header = this.block.find('.header-content');
+    const headerButton = this.block.find('.header-button a');
+    const tl = gsap.timeline({repeat:0, delay: 0});
+    const title = new SplitText(header.find('h3'), {type:"words,chars"});
+    const chars = title.words;
+
+    tl.fromTo(header.find('h2'), {alpha: 1, scale:4, y:'-200%', color:'rgba(255, 255,255, 1)'}, {alpha:1,scale:1, y:'0%', duration:5})
+    tl.from(chars, {opacity:0, scaleY: 0, y:80, duration: 0.8,  ease:Expo.easeOut, stagger: 0.1});
+
+
+    const anim = new ScrollMagic.Scene({
+      triggerElement: `#trigger-${id}`,
+      triggerHook: "onEnter",
+      duration: "100%",
+    })
+
+     .addIndicators({
+       name: `Video Intro ${this.id}`,
+       colorTrigger: "blue",
+       colorStart: "orange",
+       colorEnd: "black"
+     })
+
+     .setTween(tl)
+
+     .addTo(this.controller)
   }
 
 }
