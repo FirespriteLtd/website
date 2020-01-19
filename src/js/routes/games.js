313097adb9eb6bb0d072eca024f5ddfad1e46040
nolafs <Sontag2010>
2020-01-19 19:14:27 +0000

    - block animation performance

--- src/js/routes/games.js
+++ src/js/routes/games.js
@@ -1,15 +1,8 @@
-import $ from 'jquery'
-// import ScrollMagic from "scrollmagic/scrollmagic/minified/ScrollMagic.min";
-// import animations from "../util/animations";
-import Rellax from "rellax/rellax.min";
-import 'slick-carousel';
-import SectionParallax from "../util/sectionParallax";
-import HeaderBlock from "../util/headerBlock";
-
+import AnchorNav from "../util/anchor-nav";
 
 export default {
     init() {
-        console.log()
+        new AnchorNav('#sub-nav');
     },
     finalize() {
         // JavaScript to be fired on all pages, after page specific JS is fire
@@ -22,7 +15,6 @@ export default {
         for(let x=0; x < ($('#container-scroll > div').length -1); x++) {
             sections.push(x);
         };
-        console.log('Sections', sections.length, sections)
 
         setTimeout(()=> {
             section.init(['header', ...sections]);
