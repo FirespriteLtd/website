3469c704ef6d59c6bf097b0e1305b55355c5101d
nolafs <Sontag2010>
2020-01-22 12:02:05 +0000

    - fix news block height on mobile

--- src/js/util/sectionParallax.js
+++ src/js/util/sectionParallax.js
@@ -8,24 +8,26 @@ class SectionParallax {
   }
 
   controller() {
-   this.controller = new ScrollMagic.Controller({refreshInterval:10, container: '#container-scroll'});
+   this.controller = new ScrollMagic.Controller({refreshInterval:200, container: '#container-scroll'});
    return this.controller;
   }
 
   init(sectionArr){
+
    for(let i=0;i<sectionArr.length;i++) {
 
     const tl = gsap.timeline({repeat:0});
-    tl.fromTo(`#section-${sectionArr[i]}`,{x:'0%'}, {y:'100%', z:0, force3d: false, duration: 20, overwrite:true,  ease: Linear.easeInOut})
+    tl.to(`#section-${sectionArr[i]}`, {z:-2000,scale: 3,  force3d: true, duration: 20, overwrite:false,  ease: Linear.easeInOut})
 
     new ScrollMagic.Scene({
      triggerElement:`#trigger-${sectionArr[i]}`,
-     triggerHook: 0,
+     triggerHook: 0.5,
      duration: '200%'
     })
      .setTween(tl)
      .addTo(this.controller);
    }
+
   }
 
   getVersion(){
