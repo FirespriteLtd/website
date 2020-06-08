87d1e7a4403341f753926979b450432adfd4d755
nolafs <Sontag2010>
2020-06-08 15:16:30 +0100

    - update hugo version

--- src/js/routes/careers.js
+++ src/js/routes/careers.js
@@ -3,6 +3,7 @@ import 'slick-carousel';
 import SectionParallax from "../util/sectionParallax";
 import HeaderBlock from "../util/headerBlock";
 import SideBarController from "../util/sideBarController";
+import {gsap, Power2} from "gsap/all";
 
 export default {
     init() {
@@ -14,7 +15,7 @@ export default {
         const controller = section.controller();
         const master = new HeaderBlock(controller);
 
-        setTimeout(()=> {
+        setTimeout(() => {
             section.init(['header']);
             new SideBarController('.social-menu');
         }, 1000);
@@ -28,5 +29,48 @@ export default {
             fade: true,
             autoplaySpeed: 5000,
         });
+
+        this.buttons = document.querySelectorAll('[data-job]');
+        this.buttonClose = document.querySelector('.overlay-close');
+        for (const bt of this.buttons) {
+            bt.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.open(bt.dataset.job)
+            });
+        }
+
+        this.buttonClose.addEventListener('click', (e) => {
+            console.log('click')
+            this.close();
+        });
+    }
+    ,
+    open ($id) {
+        console.log($id)
+        const jobs = window.jobData;
+        let job;
+        jobs.forEach((v) => {
+            if($id === v.id){
+                job = v;
+            }
+        })
+
+        console.log('job', job);
+        const popup = document.querySelector('.overlay-popup')
+
+        const header = popup.querySelector('.overlay-header')
+        const content = popup.querySelector('.content')
+        header.append(job.title);
+        content.innerHTML = `${job.content}<a href="mailto:${job.email}?subject=${job.title}" class="hollow button m-t-30">Apply</a>`;
+        popup.classList.add('is-shown');
+        gsap.fromTo(popup, { y: '100%'}, {y: '0', duration: 1, ease: Power2.easeInOut})
     },
+
+    close () {
+        console.log('TEST')
+        const popup = document.querySelector('.overlay-popup')
+        gsap.to(popup , {y: '100%', duration: 1, ease: Power2.easeInOut, onComplete:()=>{
+                popup.classList.remove('is-shown');
+            }})
+    }
 };
