464a3920ef945ae82d9dd8cb33c0db2ea3912911
nolafs <Sontag2010>
2020-06-10 16:12:10 +0100

    - update hugo version

--- knowledgeApp/knowledge/src/app/services/gamesbyplatform.pipe.ts
+++ knowledgeApp/knowledge/src/app/services/gamesbyplatform.pipe.ts
@@ -7,7 +7,7 @@ export class GamesbyplatformPipe implements PipeTransform {
 
   transform(value: any, platform): any {
     if(!platform || platform === 'all'){
-      return value;
+      return  value.filter(item => item.platforms.length === 0);
     }
 
     return value.filter(item => item.platforms.includes(platform));
